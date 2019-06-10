98f3d4c0ec9e26eb523213406ac69963361c734d
stepseazy <hb31@cornell.edu>
2019-06-10 11:07:22 -0700

    basically ready for popup loft

--- src/components/PayButton.js
+++ src/components/PayButton.js
@@ -1,8 +1,50 @@
 import React from "react";
+
+import { API } from "aws-amplify";
+
+import StripeCheckout from "react-stripe-checkout";
+
 // import { Notification, Message } from "element-react";
 
-const PayButton = () => {
-  return <div>PayButton</div>;
+const stripeConfig = {
+  currency: "USD",
+  publishableAPIKey: "pk_test_u5EoEcFPSmGReZvAjxmf9Zb8"
+};
+
+const PayButton = ({ product, user }) => {
+  const handleCharge = async token => {
+    try {
+      const result = await API.post("api4c0d9f8e", "/charge", {
+        //amplifyagorab7b84710
+        body: {
+          token,
+          charge: {
+            currency: stripeConfig.currency,
+            amount: product.price,
+            description: product.description
+          }
+        }
+      });
+      console.log(result);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <StripeCheckout
+      token={handleCharge}
+      email={user.attributes.email}
+      name={product.description}
+      amount={product.price}
+      currency={stripeConfig.currency}
+      stripeKey={stripeConfig.publishableAPIKey}
+      shippingAddress={product.shipped}
+      billingAddress={product.shipped}
+      locale="auto"
+      allowRememberMe={true}
+    />
+  );
 };
 
 export default PayButton;
